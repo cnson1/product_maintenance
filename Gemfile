2c735cc86d418e3c889e3a6a9aa87b548da62d0e
son.cao <son.cao@executionlab.asia>
2022-05-06 15:58:34 +0700

    login + admin

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.1.2"
 
+gem 'bcrypt', '~> 3.1.7'
+
 gem 'sassc-rails'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'admin/index'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :logout
+  end
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  resources :users
   root 'store#index', as: 'store_index'
   resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
